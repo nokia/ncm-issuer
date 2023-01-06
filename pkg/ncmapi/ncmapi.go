e356555a165b95c39ef764ab77148f2c5f751c1b
raczu <raczuekk@gmail.com>
2023-01-06 14:47:12 +0100

    Finished NCM API Client

--- pkg/ncmapi/ncmapi.go
+++ pkg/ncmapi/ncmapi.go
@@ -1,14 +1,17 @@
 package ncmapi
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -41,6 +44,9 @@ type Client struct {
 	// a certain time period), there is an address of a second server to which client can send the
 	// same request
 	allowRetry bool
+
+	// Determines whether the profile ID should be used during a certificate renewal operation
+	useProfileIDforRenew bool
 
 	HTTPClient *http.Client
 	Log        logr.Logger
@@ -118,13 +124,14 @@ func NewClient(ncmConfig *controllers.NcmConfig, log logr.Logger) (*Client, erro
 	}
 
 	c := &Client{
-		ncmServer:  ncmConfig.NcmSERVER,
-		ncmServer2: ncmConfig.NcmSERVER2,
-		allowRetry: ncmConfig.NcmSERVER2 != "",
-		user:       ncmConfig.Username,
-		password:   ncmConfig.UsrPassword,
-		HTTPClient: HTTPClient,
-		Log:        log,
+		ncmServer:            ncmConfig.NcmSERVER,
+		ncmServer2:           ncmConfig.NcmSERVER2,
+		allowRetry:           ncmConfig.NcmSERVER2 != "",
+		user:                 ncmConfig.Username,
+		password:             ncmConfig.UsrPassword,
+		useProfileIDforRenew: ncmConfig.UseProfileIDForRenew,
+		HTTPClient:           HTTPClient,
+		Log:                  log,
 	}
 
 	return c, nil
@@ -177,27 +184,21 @@ func configureHTTPClient(ncmConfig *controllers.NcmConfig) (*http.Client, error)
 	return HTTPClient, nil
 }
 
-func (c *Client) newRequest(method, path string, extraParams map[string]string) (*http.Request, error) {
+func (c *Client) newRequest(method, path string, body io.Reader) (*http.Request, error) {
 	ncmServerURL, err := url.Parse(c.ncmServer)
 	if err != nil {
 		return nil, &ClientError{Type: "URL parsing error", Message: err}
 	}
 
 	ncmServerURL.Path = path
-	params := url.Values{}
 
-	if extraParams != nil {
-		for k, v := range extraParams {
-			params.Add(k, v)
-		}
-	}
-
-	req, err := http.NewRequest(method, ncmServerURL.String(), strings.NewReader(params.Encode()))
+	req, err := http.NewRequest(method, ncmServerURL.String(), body)
 	if err != nil {
 		return nil, &ClientError{Type: "http.NewRequest creation error", Message: err}
 	}
 
 	req.SetBasicAuth(c.user, c.password)
+	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Accept-Language", "en_US")
 
 	return req, nil
@@ -205,15 +206,14 @@ func (c *Client) newRequest(method, path string, extraParams map[string]string) 
 
 func (c *Client) retryRequest(req *http.Request) (*http.Response, error) {
 	c.Log.V(1).Info("retrying request to the second NCM EXTERNAL API")
-	ncmServer2URL, err := url.Parse(c.ncmServer2)
 
+	ncmServer2URL, err := url.Parse(c.ncmServer2)
 	if err != nil {
 		return nil, &ClientError{Type: "URL parsing error", Message: err}
 	}
 
 	req.URL.Host = ncmServer2URL.Host
 	resp, err := c.HTTPClient.Do(req)
-
 	if err != nil {
 		return nil, &ClientError{Type: "http.Client.Do error", Message: err}
 	}
@@ -224,40 +224,34 @@ func (c *Client) retryRequest(req *http.Request) (*http.Response, error) {
 func (c *Client) validateResponse(resp *http.Response) ([]byte, error) {
 	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 		body, err := io.ReadAll(resp.Body)
-
 		if err != nil {
 			return nil, &ClientError{Type: "response body read error", Message: err}
 		}
 
 		return body, nil
 
-	} else {
-		body, err := io.ReadAll(resp.Body)
-
-		if err != nil {
-			return nil, &ClientError{Type: "response body read error", Message: err}
-		}
-
-		apiError := APIError{}
-
-		err = json.Unmarshal(body, &apiError)
-
-		if err != nil {
-			return nil, &ClientError{Type: "json unmarshal error", Message: err}
-		}
-
-		return nil, &apiError
 	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, &ClientError{Type: "response body read error", Message: err}
+	}
+
+	apiError := APIError{}
+	err = json.Unmarshal(body, &apiError)
+	if err != nil {
+		return nil, &ClientError{Type: "json unmarshal error", Message: err}
+	}
+
+	return nil, &apiError
 }
 
 func (c *Client) doRequest(req *http.Request) (*http.Response, error) {
 	resp, err := c.HTTPClient.Do(req)
-
 	if err != nil && os.IsTimeout(err) && c.allowRetry {
-		c.Log.V(1).Info("connection timeout exceeded while connection to the main NCM EXTERNAL API")
+		c.Log.V(1).Info("connection timeout exceeded while connecting to the main NCM EXTERNAL API")
 
 		resp, err := c.retryRequest(req)
-
 		if err != nil {
 			return nil, err
 		}
@@ -272,15 +266,13 @@ func (c *Client) doRequest(req *http.Request) (*http.Response, error) {
 }
 
 func (c *Client) GetCAs() (CAsResponse, error) {
-	req, err := c.newRequest("GET", CAsURL, nil)
-	req.Header.Set("Accept", "application/json")
-
+	params := url.Values{}
+	req, err := c.newRequest(http.MethodGet, CAsURL, strings.NewReader(params.Encode()))
 	if err != nil {
 		return CAsResponse{}, err
 	}
 
 	resp, err := c.doRequest(req)
-
 	if err != nil {
 		return CAsResponse{}, err
 	}
@@ -288,14 +280,12 @@ func (c *Client) GetCAs() (CAsResponse, error) {
 	defer resp.Body.Close()
 
 	body, err := c.validateResponse(resp)
-
 	if err != nil {
 		return CAsResponse{}, err
 	}
 
 	cas := CAsResponse{}
 	err = json.Unmarshal(body, &cas)
-
 	if err != nil {
 		return CAsResponse{}, &ClientError{Type: "json unmarshal error", Message: err}
 	}
@@ -305,25 +295,223 @@ func (c *Client) GetCAs() (CAsResponse, error) {
 }
 
 func (c *Client) GetCA(path string) (CAResponse, error) {
+	params := url.Values{}
+	req, err := c.newRequest(http.MethodGet, CAsURL+path, strings.NewReader(params.Encode()))
+	if err != nil {
+		return CAResponse{}, err
+	}
 
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return CAResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := c.validateResponse(resp)
+	if err != nil {
+		return CAResponse{}, err
+	}
+
+	ca := CAResponse{}
+	err = json.Unmarshal(body, &ca)
+	if err != nil {
+		return CAResponse{}, &ClientError{Type: "json unmarshal error", Message: err}
+	}
+
+	return ca, nil
 }
 
 func (c *Client) SendCSR(pem []byte, CA CAResponse, profileId string) (CSRResponse, error) {
+	filePath, err := writePemToTempFile("/tmp/ncm", pem)
+	if err != nil {
+		return CSRResponse{}, &ClientError{Type: "writing file error", Message: err}
+	}
 
+	file, err := os.Open(filePath)
+	if err != nil {
+		return CSRResponse{}, &ClientError{Type: "opening file error", Message: err}
+	}
+
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("pkcs10", filepath.Base(filePath))
+	if err != nil {
+		return CSRResponse{}, &ClientError{Type: "writer error", Message: err}
+	}
+
+	params := map[string]string{
+		"ca": CA.Href,
+	}
+
+	if profileId != "" {
+		params["profileId"] = profileId
+	}
+
+	for k, v := range params {
+		_ = writer.WriteField(k, v)
+	}
+
+	_, _ = io.Copy(part, file)
+
+	err = writer.Close()
+	if err != nil {
+		return CSRResponse{}, &ClientError{Type: "writer error", Message: err}
+	}
+
+	req, err := c.newRequest(http.MethodPost, CSRURL, body)
+	if err != nil {
+		return CSRResponse{}, err
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	err = os.Remove(filePath)
+	if err != nil {
+		return CSRResponse{}, &ClientError{Type: "removing file error", Message: err}
+	}
+
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return CSRResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	respBody, err := c.validateResponse(resp)
+	if err != nil {
+		return CSRResponse{}, err
+	}
+
+	csr := CSRResponse{}
+	err = json.Unmarshal(respBody, &csr)
+	if err != nil {
+		return CSRResponse{}, &ClientError{Type: "json unmarshal error", Message: err}
+	}
+
+	return csr, nil
 }
 
 func (c *Client) CheckCSRStatus(path string) (CSRStatusResponse, error) {
+	params := url.Values{}
+	req, err := c.newRequest(http.MethodGet, path, strings.NewReader(params.Encode()))
+	if err != nil {
+		return CSRStatusResponse{}, err
+	}
 
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return CSRStatusResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := c.validateResponse(resp)
+	if err != nil {
+		return CSRStatusResponse{}, err
+	}
+
+	csrStatus := CSRStatusResponse{}
+	err = json.Unmarshal(body, &csrStatus)
+	if err != nil {
+		return CSRStatusResponse{}, &ClientError{Type: "json unmarshal error", Message: err}
+	}
+
+	return csrStatus, nil
 }
 
 func (c *Client) DownloadCertificate(path string) (CertificateDownloadResponse, error) {
+	params := url.Values{}
+	req, err := c.newRequest(http.MethodGet, path, strings.NewReader(params.Encode()))
+	if err != nil {
+		return CertificateDownloadResponse{}, err
+	}
 
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return CertificateDownloadResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := c.validateResponse(resp)
+	if err != nil {
+		return CertificateDownloadResponse{}, err
+	}
+
+	certificate := CertificateDownloadResponse{}
+	err = json.Unmarshal(body, &certificate)
+	if err != nil {
+		return CertificateDownloadResponse{}, &ClientError{Type: "json unmarshal error", Message: err}
+	}
+
+	return certificate, nil
 }
 
-func (c *Client) DownloadCertificateInPEM(path string) (CertificateDownloadResponse, error) {
+func (c *Client) DownloadCertificateInPEM(path string) ([]byte, error) {
+	params := url.Values{}
+	req, err := c.newRequest(http.MethodGet, path, strings.NewReader(params.Encode()))
+	if err != nil {
+		return nil, err
+	}
 
+	req.Header.Set("Accept", "application/x-pem-file")
+
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := c.validateResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
 
-func (c *Client) RenewCertificate(path string, certDuration metav1.Duration) (RenewCertificateResponse, error) {
+func (c *Client) RenewCertificate(path string, certDuration metav1.Duration, profileId string) (RenewCertificateResponse, error) {
+	notBefore := time.Now()
+	notAfter := notBefore.Add(certDuration.Duration)
 
+	newData := map[string]string{
+		"notBefore": notBefore.Format(time.RFC3339Nano),
+		"notAfter":  notAfter.Format(time.RFC3339Nano),
+	}
+
+	if profileId != "" && c.useProfileIDforRenew {
+		newData["profileId"] = profileId
+	}
+
+	jsonData, _ := json.Marshal(&newData)
+	req, err := c.newRequest(http.MethodPost, path+"/update", strings.NewReader(string(jsonData)))
+	if err != nil {
+		return RenewCertificateResponse{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.doRequest(req)
+	if err != nil {
+		return RenewCertificateResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := c.validateResponse(resp)
+	if err != nil {
+		return RenewCertificateResponse{}, err
+	}
+
+	renewedCertificate := RenewCertificateResponse{}
+	err = json.Unmarshal(body, &renewedCertificate)
+	if err != nil {
+		return RenewCertificateResponse{}, &ClientError{Type: "json unmarshal error", Message: err}
+	}
+
+	return renewedCertificate, nil
 }
