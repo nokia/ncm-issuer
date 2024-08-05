860afedc8892a1a82fb3ca5d5f7deb6ce4b585e5
locohamster <locohamster+ncm@gmail.com>
2024-08-05 16:22:10 +0200

    Fix: add missing rbac roles

--- pkg/controllers/issuer_controller.go
+++ pkg/controllers/issuer_controller.go
@@ -62,6 +62,7 @@ func (r *IssuerReconciler) newIssuer() (client.Object, error) {
 //+kubebuilder:rbac:groups=certmanager.ncm.nokia.com,resources=issuers;clusterissuers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=certmanager.ncm.nokia.com,resources=issuers/status;clusterissuers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=events,verbs=create;list;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
